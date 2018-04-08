8d07b346f8a1cce94d14a22cdb849d5eacb2b091
Paul Asmuth <paul@asmuth.com>
2018-04-08 17:09:52 +0200

    test-runnner: print log of test failures

--- test/test-runner.sh
+++ test/test-runner.sh
@@ -5,6 +5,7 @@ source test/test-util.sh
 num_total=0
 num_passed=0
 num_failed=0
+failed_logs=()
 
 print_yellow "Running tests..."
 echo
@@ -21,9 +22,17 @@ for f in $(cd "${TEST_SRCDIR}" && find . -name "testcase-*" -type f); do
     print_red "FAIL"
     echo
     num_failed=$[ $num_failed + 1 ]
+    failed_logs+=($f.log)
   fi
 done
 echo
+
+for failed in ${failed_logs[@]}; do
+  print_red "FAIL: "
+  echo "${failed}"
+  sed -e 's/^/  | /' < "${TEST_TMPDIR}/${failed}"
+  echo
+done
 
 print_yellow "Test Summary: "
 if [[ ${num_passed} -eq ${num_total} && ${num_total} -gt 0 ]]; then
