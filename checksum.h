5988cc0d2369b3510572515ef1f1651ce44529ed
Paul Asmuth <paul@asmuth.com>
2024-03-23 12:15:23 +0100

    add the Checksum{,Type,Value} structs

--- checksum.h
+++ checksum.h
@@ -1,4 +1,18 @@
 #pragma once
 #include <string>
 
+enum class ChecksumType {
+  MD5,
+  SHA1,
+};
+
+struct ChecksumValue {
+  std::string data;
+};
+
+struct Checksum {
+  ChecksumType type;
+  ChecksumValue value;
+};
+
 std::string checksum_compute_sha1(const std::string& file_path);
