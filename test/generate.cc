8e45d410589637d7605f504ef0e3faefcdd8ebba
Paul Asmuth <paul@asmuth.com>
2024-03-24 13:33:53 +0100

    add test_generate_zeroes

--- test/generate.cc
+++ test/generate.cc
@@ -5,9 +5,23 @@ void test_create_file(
   const std::filesystem::path& file_path,
   const std::string& file_data
 ) {
-
   std::ofstream file_writer(file_path);
   file_writer << file_data;
+
+  if (file_writer.bad()) {
+    throw std::runtime_error("error while writing test file: " + file_path.string());
+  }
+}
+
+void test_create_file(
+  const std::filesystem::path& file_path,
+  size_t file_size,
+  TestDataGenerator file_data
+) {
+  std::ofstream file_writer(file_path);
+  for (size_t i = 0; i < file_size; ++i) {
+    file_writer << file_data(i);
+  }
 
   if (file_writer.bad()) {
     throw std::runtime_error("error while writing test file: " + file_path.string());
@@ -20,4 +34,8 @@ void test_create_directory(
   if (!std::filesystem::create_directory(file_path)) {
     throw std::runtime_error("error while creating test directory: " + file_path.string());
   }
+}
+
+TestDataGenerator test_generate_zeroes() {
+  return [] (auto i) { return 0; };
 }
--- test/generate.h
+++ test/generate.h
@@ -1,11 +1,23 @@
 #pragma once
 #include <filesystem>
+#include <functional>
+
+using TestDataGenerator = std::function<uint8_t (size_t)>;
 
 void test_create_file(
   const std::filesystem::path& file_path,
   const std::string& file_data
 );
 
+void test_create_file(
+  const std::filesystem::path& file_path,
+  size_t file_size,
+  TestDataGenerator file_data
+);
+
 void test_create_directory(
   const std::filesystem::path& file_path
 );
+
+TestDataGenerator test_generate_zeroes();
+
