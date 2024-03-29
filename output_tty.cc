6317770701e2d0645aa0e5288965418b9bd0a310
Paul Asmuth <paul@asmuth.com>
2024-03-29 13:43:28 +0100

    add tty_print_value_bytes

--- output_tty.cc
+++ output_tty.cc
@@ -13,3 +13,16 @@ std::string tty_print_error(const std::string& s) {
   return fmt::format("\033[1;41m{}\033[0m", s);
 }
 
+std::string tty_print_value_bytes(uint64_t x) {
+  if (x < 1'000) {
+    return fmt::format("{}B", x);
+  } else if (x < 1'000'000) {
+    return fmt::format("{:.3f}KB", x / double(1'000));
+  } else if (x < 1'000'000'000) {
+    return fmt::format("{:.3f}MB", x / double(1'000'000));
+  } else if (x < 1'000'000'000'000) {
+    return fmt::format("{:.3f}GB", x / double(1'000'000'000));
+  } else {
+    return fmt::format("{:.3f}TB", x / double(1'000'000'000'000));
+  }
+}
--- output_tty.h
+++ output_tty.h
@@ -4,3 +4,4 @@
 std::string tty_print_success(const std::string& s);
 std::string tty_print_warning(const std::string& s);
 std::string tty_print_error(const std::string& s);
+std::string tty_print_value_bytes(uint64_t x);
